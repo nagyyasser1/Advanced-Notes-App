9acc32a26e40f76951c89ef7340709ef3c7e48a5
nagy <nagyy8751@gmail.com>
2023-08-03 20:54:37 +0200

    v1

--- middleware/logger.js
+++ middleware/logger.js
@@ -1,11 +1,28 @@
-const { format } = require("date-fns");
 const { v4: uuid } = require("uuid");
 const fs = require("fs");
 const fsPromises = require("fs").promises;
 const path = require("path");
 
+function formatDateTimeToYYYYMMDDHHMMSS(date) {
+  if (!(date instanceof Date) || isNaN(date)) {
+    return "Invalid date";
+  }
+
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, "0");
+  const day = String(date.getDate()).padStart(2, "0");
+
+  const hours = String(date.getHours()).padStart(2, "0");
+  const minutes = String(date.getMinutes()).padStart(2, "0");
+  const seconds = String(date.getSeconds()).padStart(2, "0");
+
+  const formattedDateTime = `${year}/${month}/${day} ${hours}:${minutes}:${seconds}`;
+
+  return formattedDateTime;
+}
+
 const logEvents = async (message, logFileName) => {
-  const dateTime = format(new Date(), "yyyyMMdd\tHH:mm:ss");
+  const dateTime = formatDateTimeToYYYYMMDDHHMMSS(new Date());
   const logItem = `${dateTime}\t${uuid()}\t${message}\n`;
 
   try {
--- server.js
+++ server.js
@@ -1,16 +1,28 @@
+require("dotenv").config();
 const express = require("express");
 const path = require("path");
-const { logger } = require("./middleware/logger");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const connectDB = require("./config/dbConn");
+const mongoose = require("mongoose");
+const corsOptions = require("./config/corsOptions");
+const { logger, logEvents } = require("./middleware/logger");
+const errorHandler = require("./middleware/errorHandler");
 
 const app = express();
 const PORT = process.env.PORT || 3500;
 
+connectDB();
 app.use(logger);
+app.use(cors(corsOptions));
 app.use(express.json());
+app.use(cookieParser());
 app.use("/", express.static(path.join(__dirname, "public")));
 app.use("/", require("./routes/route"));
+app.use("/users", require("./routes/userRoutes"));
+app.use("/notes", require("./routes/noteRoutes"));
 
-app.all("*", (req, res) => {
+app.all("*", (req, res, next) => {
   res.status(404);
   if (req.accepts("html")) {
     res.sendFile(path.join(__dirname, "views", "404.html"));
@@ -20,6 +32,19 @@ app.all("*", (req, res) => {
     res.type("txt").send("404 Not Found!");
   }
 });
-app.listen(PORT, () => {
-  console.log(`server running on port: ${PORT}`);
+app.use(errorHandler);
+
+mongoose.connection.once("open", () => {
+  console.log("Connected To MongoDB");
+  app.listen(PORT, () => {
+    console.log(`server running on port: ${PORT}`);
+  });
+});
+
+mongoose.connection.on("error", (err) => {
+  console.log(err);
+  logEvents(
+    `${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`,
+    "mongoErrLog.log"
+  );
 });
